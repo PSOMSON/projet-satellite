d5d102f727d01ed0631193b3734b759d6830fe95
cecelaplante <celeste.enigrand@gmail.com>
2023-12-20 15:52:31 +0100

    mini changements dans le main

--- python_files/main.py
+++ python_files/main.py
@@ -1,11 +1,11 @@
-import NetworkX as nx
+import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
 import openGraph as og
 import graphDeg as gd
-import graphClique.py as gc
-import graphPath.py as gp
+import graphClique as gc
+import graphPath as gp
 
 G = og.graphFromCSV(og.FILES[0],distance_limit=40000)
 print("import du graph finis...")
