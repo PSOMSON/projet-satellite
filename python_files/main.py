2b9578c3541575a11f0f1a5ae02c318a230f7ae1
cecelaplante <celeste.enigrand@gmail.com>
2024-01-26 20:01:03 +0100

    finis

--- python_files/main.py
+++ python_files/main.py
@@ -13,9 +13,17 @@ IDX = 1
 G = og.graphFromCSV(og.FILES[IDX],distance_limit=distance_limite,valuated=True)
 print("import du graph finis...")
 
+og.plotGraph(G, "graph n°"+str(IDX)+", distance limite : "+str(distance_limite))
+
+
+# -----------------degrés------------
+gd.calcul_degre_degre_clustering(G) 
+
+#On dessine la distribution des degrés et du degré de clustering
+gd.draw_degree_and_clustering_distributions(G)
+
 # ----------------plus courts chemins----------
 
-og.plotGraph(G, "graph n°"+str(IDX)+", distance limite : "+str(distance_limite))
 gp.path_analyse(G, True)
 
 # -----------------cliques et composantes connexes------------
@@ -33,8 +41,4 @@ gc.histconnnectedComponents(G)
 cc = gc.bigest_connectedComponents(G)
 og.plotGraph(G, "plus grande composante connexe",cc)
 
-# -----------------degrés------------
-gd.calcul_degre_degre_clustering(G) 
 
-#On dessine la distribution des degrés et du degré de clustering
-gd.draw_degree_and_clustering_distributions(G)
